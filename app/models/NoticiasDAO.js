5ff98c1f34b33272fc36aae88e1f238a1790453a
rafael <rafael.machado@camara.gov.br>
2016-11-04 14:44:09 -0200

    mais

---
+++ app/models/NoticiasDAO.js
@@ -0,0 +1,23 @@
+function NoticiasDAO(connection){
+  this._connection = connection;
+}
+
+NoticiasDAO.prototype.getNoticias = function(callback){
+    this._connection.query('select * from noticias',callback);
+};
+
+NoticiasDAO.prototype.getNoticia = function(callback){
+    this._connection.query('select * from noticias where id_noticia = 1',callback);
+};
+
+NoticiasDAO.prototype.salvarNoticia =  function(noticia,callback){
+    this._connection.query('insert into noticias set ? ',noticia,callback);
+};
+
+NoticiasDAO.prototype.get5UltimasNoticias =  function(callback){
+    this._connection.query('select * from noticias order by data_criacao desc limit 5 ',callback);
+};
+
+module.exports = function(){
+  return NoticiasDAO;
+};
